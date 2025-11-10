918cba9c6af893c86f8850c187bf75b34be5b02d
Coleman Scott <coleman.scott98@gmail.com>
2025-11-10 11:03:41 -0500

    React

---
+++ src/data/projects.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    title: "Find My Library",
+    description: "A React app using Google Maps to locate outdoor libraries.",
+    link: "https://github.com/colemanscott98/find-my-library"
+  },
+  {
+    title: "Task Manager",
+    description: "A serverless AWS task manager for portfolio demonstration.",
+    link: "https://github.com/colemanscott98/aws-task-app"
+  }
+];
